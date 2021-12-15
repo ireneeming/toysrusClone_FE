caa3f4efb2598a86a02b32ef3d26d1db56707f51
jawoon816 <jawoon816@gmail.com>
2021-12-15 16:05:10 +0900

    jawoon

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,8 @@
-import axios from 'axios';
+import axios from 'axios'
 
 const api = axios.create({
-  baseURL: "http://3.38.98.44"
+  baseURL: 'http://13.125.197.66',
   //baseURL: "http://localhost:3500"
-});
+})
 
-export default api;
-
+export default api
